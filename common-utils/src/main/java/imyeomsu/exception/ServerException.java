5abc7f3f66a972c28e487eea94047a858bac18da
hwiju yeom <yeom456@gmail.com>
2024-04-06 18:22:02 +0900

    Feat: 메이븐을 이용한 새마음 새시작

---
+++ common-utils/src/main/java/imyeomsu/exception/ServerException.java
@@ -0,0 +1,31 @@
+package imyeomsu.exception;
+
+import common_utils.common.code.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+/*
+* 500 - 서버 예외시 사용합니다.
+* */
+@Getter
+public class ServerException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+    private final HttpStatus responseStatus;
+    private final String displayErrorMessage;
+
+
+    public ServerException(ErrorCode errorCode, String message) {
+        this(errorCode, errorCode.getName(), message);
+    }
+    public ServerException(ErrorCode errorCode, String message,
+                           String displayErrorMessage) {
+        super(message);
+        this.errorCode = errorCode;
+        this.responseStatus = errorCode.getResponseStatus();
+        this.displayErrorMessage = displayErrorMessage;
+
+    }
+
+}
